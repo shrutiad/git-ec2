284cb03e2ad906b289b1e5ebf810ab81cba701c7
shrutiad <109783667+shrutiad@users.noreply.github.com>
2022-09-01 11:12:44 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "ec2_demo" {
-  ami           = "ami-0ff89c4ce7de192ea"
+  ami           = "ami-0b89f7b3f054b957e"
   instance_type = "t2.micro"
   tags = {
     Name = "Created_By_Terraform/jenkins"
